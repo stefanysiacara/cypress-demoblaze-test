f8cd73d7a524e2ba0a914f6a458d872d43cfa6f9
Stefany.Siacara <stefany.siacara@jalasoft.com>
2021-10-22 12:31:59 -0400

    login test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,12 +1,17 @@
 describe('Login Page', () => {
+    const USER_NAME = 'rcgongora';
+    const USER_PASSWORD = '171183';
+    beforeEach(() => {
+        cy.visit('/');
+        cy.get('a#login2').click();
+        cy.get('input#loginusername').type(USER_NAME);
+        cy.wait(2000);
+        cy.get('input#loginpassword').type(USER_PASSWORD); 
+        cy.wait(2000);
+        cy.contains('button.btn-primary','Log in').click();
+    });
     it('verify we can login using valid credentials', () => {
-       cy.visit('/');
-       cy.get('a#login2').click();
-       cy.get('input#loginusername').type('rcgongorad');
-       cy.wait(2000);
-       cy.get('input#loginpassword').type('171183'); 
-       cy.wait(2000);
-       cy.contains('button.btn-primary','Log in').click();
-       cy.contains('Welcome rcgongora');
+        cy.get('#nameofuser').should('be.visible');
+        cy.contains('Welcome ' + USER_NAME);
     });
 });

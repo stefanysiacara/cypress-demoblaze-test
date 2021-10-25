b821b76bf9796e8290948beff03d3ca4292ed761
Stefany.Siacara <stefany.siacara@jalasoft.com>
2021-10-25 11:59:15 -0400

    spaces update

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,17 +1,16 @@
 describe('Login Page', () => {
-    const USER_NAME = 'rcgongora';
-    const USER_PASSWORD = '171183';
-    beforeEach(() => {
-        cy.visit('/');
-        cy.get('a#login2').click();
-        cy.get('input#loginusername').type(USER_NAME);
-        cy.wait(2000);
-        cy.get('input#loginpassword').type(USER_PASSWORD); 
-        cy.wait(2000);
-        cy.contains('button.btn-primary','Log in').click();
-    });
-    it('verify we can login using valid credentials', () => {
-        cy.get('#nameofuser').should('be.visible');
-        cy.contains('Welcome ' + USER_NAME);
-    });
+	const USER_NAME = 'florecita';
+	const USER_PASSWORD = '1234567';
+	beforeEach(() => {
+		cy.visit('/');
+		cy.get('a#login2').click();
+		cy.get('input#loginusername').should('be.visible');
+		cy.get('input#loginusername').focus().type(USER_NAME).blur();
+		cy.get('input#loginpassword').type(USER_PASSWORD); 
+		cy.contains('button.btn-primary','Log in').click();
+	});
+	it('verify we can login using valid credentials', () => {
+		cy.get('#nameofuser').should('be.visible');
+		cy.contains('Welcome ' + USER_NAME);
+	});
 });

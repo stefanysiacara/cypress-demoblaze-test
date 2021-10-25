f11406267b9d7a53fe7d0f3a5c211b6fdecd2dd3
Stefany.Siacara <stefany.siacara@jalasoft.com>
2021-10-25 12:21:56 -0400

    spaces update2

--- cypress/integration/createUser.spec.js
+++ cypress/integration/createUser.spec.js
@@ -1,10 +1,10 @@
 describe('New User', () => {
-    it('Verify we can create a new user', () => {
-        cy.visit('/');
-        cy.get('a#signin2').click();
-        cy.get('input#sign-username').should('be.visible');
-        cy.get('input#sign-username').focus().type('florecita');
-        cy.get('input#sign-password').type('1234567');
-        cy.contains('button.btn-primary', 'Sign up').click();
-    });
+  it('Verify we can create a new user', () => {
+    cy.visit('/');
+    cy.get('a#signin2').click();
+    cy.get('input#sign-username').should('be.visible');
+    cy.get('input#sign-username').focus().type('florecita');
+    cy.get('input#sign-password').type('1234567');
+    cy.contains('button.btn-primary', 'Sign up').click();
+  });
 });
